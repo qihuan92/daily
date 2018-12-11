9dcfb2e042dd7a9a17ee400fb4304238e4eb7b99
qihuan <qihuan92@126.com>
2018-12-11 16:18:26 +0800

    首页条目分组

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
 import 'package:daily/page/home_page.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/date_symbol_data_local.dart';
 
-void main() => runApp(HomePage());
+void main() {
+  // 初始化 intl 的默认语言
+  initializeDateFormatting('zh_CN');
+  runApp(HomePage());
+}
