ffb2dcae24f66e3050fad39b4f8f337f2c057c37
qihuan <qihuan@fengbangleasing.com>
2018-12-13 16:24:52 +0800

    修改加载样式

---
+++ lib/widget/loding_widget.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import "package:pull_to_refresh/pull_to_refresh.dart";
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+class LoadingFooterWidget extends StatelessWidget {
+  final int mode;
+
+  const LoadingFooterWidget(this.mode);
+
+  @override
+  Widget build(BuildContext context) {
+    return ClassicIndicator(
+      mode: mode,
+      idleText: '上拉加载',
+      idleIcon: Container(),
+      refreshingText: '加载中...',
+      refreshingIcon: SpinKitFadingFour(
+        color: Theme.of(context).primaryColor,
+        size: 20,
+      ),
+      failedText: '加载失败',
+      failedIcon: Icon(Icons.error, color: Colors.black38),
+      completeText: '加载完成',
+      completeIcon: Container(),
+      noDataText: '没有更多数据',
+      noMoreIcon: Container(),
+    );
+  }
+}
+
+class LoadingHeaderWidget extends StatelessWidget {
+  final int mode;
+
+  const LoadingHeaderWidget(this.mode);
+
+  @override
+  Widget build(BuildContext context) {
+    return ClassicIndicator(
+      mode: mode,
+      idleText: '下拉可刷新...',
+      idleIcon: Icon(
+        Icons.arrow_downward,
+        size: 18,
+        color: Theme.of(context).primaryColor,
+      ),
+      releaseText: '释放可刷新...',
+      releaseIcon: Icon(
+        Icons.arrow_downward,
+        size: 18,
+        color: Theme.of(context).primaryColorDark,
+      ),
+      refreshingText: '加载中...',
+      refreshingIcon: SpinKitFadingFour(
+        color: Theme.of(context).primaryColor,
+        size: 20,
+      ),
+      failedText: '加载失败',
+      failedIcon: Icon(Icons.error, color: Colors.black38),
+      completeText: '加载完成',
+      completeIcon: Container(),
+      noDataText: '没有更多数据',
+      noMoreIcon: Container(),
+    );
+  }
+}
